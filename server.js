a345a4e7d7187f8ab31a74cd37fe715cc7eeee23
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2023-07-16 03:41:27 +0800

    Installed primary dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Add your Express routes and middleware here
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('Next.Js & Express is Ready on http://localhost:3000');
+  });
+});
