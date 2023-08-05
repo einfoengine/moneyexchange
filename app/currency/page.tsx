b47571883670eb4cc8faa3db87f99a8dd627fc3a
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2023-08-06 01:15:36 +0800

    All the basic pages are done.

--- app/currency/page.tsx
+++ app/currency/page.tsx
@@ -1,8 +1,97 @@
+import Section from "@/components/section";
 import Client from "../client-template";
+
+interface Currency {
+    flag: string;
+    unit: string;
+    code: string;
+    currency: string;
+    weBuy: number;
+    weSell: number;
+    lastUpdated: string;
+    info: undefined;
+  }
+  
+  interface CurrencyListProps {
+    currencies: Currency[];
+  }
+
+  const currencies: Currency[] = [
+    {
+      flag: '🇺🇸',
+      unit: 'US Dollar',
+      code: 'USD',
+      currency: 'United States Dollar',
+      weBuy: 1.2345,
+      weSell: 1.3456,
+      lastUpdated: '2023-08-03',
+      info: undefined
+    },
+    {
+      flag: '🇪🇺',
+      unit: 'Euro',
+      code: 'EUR',
+      currency: 'Euro',
+      weBuy: 1.0987,
+      weSell: 1.2345,
+      lastUpdated: '2023-08-03',
+      info: undefined
+    },
+    {
+      flag: '🇬🇧',
+      unit: 'British Pound',
+      code: 'GBP',
+      currency: 'British Pound Sterling',
+      weBuy: 1.5678,
+      weSell: 1.6789,
+      lastUpdated: '2023-08-03',
+      info: undefined
+    },
+    // Add more currencies as needed
+  ];
+  
+
+  const CurrencyList: React.FC<CurrencyListProps> = ({ currencies }) => {
+    return (
+      <table className="w-full border border-collapse border-gray-300 rounded">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="border border-gray-300 px-4 py-2 text-left">FLAG</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">UNIT</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">CODE</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">CURRENCY</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">WE BUY</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">WE SELL</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">LAST UPDATED</th>
+            <th className="border border-gray-300 px-4 py-2 text-left">INFO</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currencies.map((currency, index) => (
+            <tr key={index}>
+              <td className="border border-gray-300 px-4 py-2">{currency.flag}</td>
+              <td className="border border-gray-300 px-4 py-2">{currency.unit}</td>
+              <td className="border border-gray-300 px-4 py-2">{currency.code}</td>
+              <td className="border border-gray-300 px-4 py-2">{currency.currency}</td>
+              <td className="border border-gray-300 px-4 py-2">{currency.weBuy}</td>
+              <td className="border border-gray-300 px-4 py-2">{currency.weSell}</td>
+              <td className="border border-gray-300 px-4 py-2">{currency.lastUpdated}</td>
+              <td className="border border-gray-300 px-4 py-2"><button className="bg-black text-white py-2 px-8 rounded">More</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  
+  
+
 const Currency = () => {
     return(
         <Client>
-            
+            <Section sectionName="currency-list" span={[12]} className="nt-currency">
+                <CurrencyList currencies={currencies}/>
+            </Section>
         </Client>
     )
 }
---
+++ app/services/page.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Section from "@/components/section";
+import TitleBuilder from "@/components/TitleBuilder";
+import Client from "../client-template";
+
+const servicesData = [
+    {
+      title: "Service 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "https://via.placeholder.com/150", // Placeholder image URL
+    },
+    {
+      title: "Service 2",
+      description: "Nulla facilisi. In hac habitasse platea dictumst.",
+      image: "https://via.placeholder.com/150", // Placeholder image URL
+    },
+    {
+      title: "Service 3",
+      description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      image: "https://via.placeholder.com/150", // Placeholder image URL
+    },
+    // Add more services as needed
+  ];
+  
+
+const ServicePage = () => {
+  return (
+    <Client>
+      <main className="nt-main p-2">
+        <Section sectionName="services" span={[12, 12]} className="pt-28">
+          <div key="nt-services-1" className="nt-services">
+            <TitleBuilder type="section" title="Our Services" message="Get our services, you will get bla bla bla. We are one of the best in Malaysia." className="text-center" />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+            {servicesData.map((service, index) => (
+              <div key={index} className="text-center">
+                <img src={service.image} className="mx-auto"/>
+                <h3 className="text-lg font-semibold mb-2">{service.title}</h3>
+                <p>{service.description}</p>
+
+              </div>
+            ))}
+          </div>
+        </Section>
+      </main>
+    </Client>
+  );
+};
+
+export default ServicePage;
