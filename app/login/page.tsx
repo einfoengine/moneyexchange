6c59252571ffb936439290830c1e1bd883d504f1
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2023-08-07 04:26:00 +0800

    Menu and footer added to the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import Client from "../client-template";
+import Section from "@/components/section";
 
 import { Button, Checkbox, Form, Input } from "antd";
 
@@ -18,47 +20,54 @@ type FieldType = {
 
 const Login = () => {
     return(
-        <div className="nt-dashboard">
-            <div className="nt-login border w-2/5 mx-auto mt-5 rounded p-5">
-                <Form
-                    name="nt-login-form"
-                    autoComplete="off"
-                    initialValues={{remember: true}}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                >
-                    <Form.Item<FieldType>
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
-                    >
-                    <Input />
-                    </Form.Item>
+        <Client>
+            <main>
+            <Section sectionName="registration" className="nt-registration" span={[12]}>
+                <div className="nt-login pt-24">
+                    <div className="nt-login border w-2/5 mx-auto mt-5 rounded p-5">
+                        <h3 className="text-lg">Login</h3>
+                        <Form
+                            name="nt-login-form"
+                            autoComplete="off"
+                            initialValues={{remember: true}}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                        >
+                            <Form.Item<FieldType>
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                            >
+                            <Input />
+                            </Form.Item>
 
-                    <Form.Item<FieldType>
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                    <Input.Password />
-                    </Form.Item>
+                            <Form.Item<FieldType>
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                            <Input.Password />
+                            </Form.Item>
 
-                    <Form.Item<FieldType>
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{ offset: 8, span: 16 }}
-                    >
-                    <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
+                            <Form.Item<FieldType>
+                            name="remember"
+                            valuePropName="checked"
+                            wrapperCol={{ offset: 8, span: 16 }}
+                            >
+                            <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
 
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" htmlType="submit" className="nt-btn-primary">
-                        Submit
-                    </Button>
-                    </Form.Item>
-                </Form>
-            </div>
-        </div>
+                            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                            <Button type="primary" htmlType="submit" className="nt-btn-primary">
+                                Submit
+                            </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </div>
+            </Section>
+            </main>
+        </Client>
     );
 }
 export default Login
