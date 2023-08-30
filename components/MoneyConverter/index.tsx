a8c96433928cf80a59af55b523764c8f2ee5bab1
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2023-08-30 18:15:09 +0800

    Money conversion calculator - Finish buy and sell calculation

--- components/MoneyConverter/index.tsx
+++ components/MoneyConverter/index.tsx
@@ -1,48 +1,59 @@
-
 'use client'
 
-import React, { useState } from "react";
+import React, { useState, useRef, useMemo } from "react";
 
-interface componentProps {
-    className?: string,
-    functionType: 'buy'|'sell',
-    data: any
+interface CurrencyDataItem {
+  code: string;
+  flag: string;
+  we_buy: number;
+  we_sell: number;
+  unit: number
 }
 
-const MoneyConverter:React.FC<componentProps> = ({className, functionType, data}) => {
-const [selected, setSelected] = useState("MY");
-const [selectedConvert, setSelectedConvert] = useState("IN");
-console.log("Currency data in convertor: ", data);
+interface ComponentProps {
+  className?: string;
+  functionType: 'buy' | 'sell';
+  data: CurrencyDataItem[];
+}
+
+const MoneyConverter: React.FC<ComponentProps> = ({ className, functionType, data }) => {
+    const [rate, setRate] = useState(0);
+    const [amount, setAmount] = useState(0);
+
   return (
-    <div className={`nt-money-converter nt-compontnt ${className}`}>
-        <form>
-            <div className="flex">
-                <div className="nt-form-col pl-2 grow">
-                    <label className="nt-label" htmlFor="nt-base-currency">How much you wanna trade?</label>
-                    <div className="border rounded p-1 flex">
-                        <input type="text" className="grow" defaultValue={1}/>
-                        <select name="nt-currency-select">
-                            {
-                                data.map((item, index)=>{
-                                    return(
-                                        <option key={`nt-country-${index}`} value={item.Code} selected={item.Code==='USD'}>
-                                            {item.Flag} {item.Code}
-                                        </option>
-                                    )
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div className="text-center">
-                        You will get <span>30</span> <span>RM</span>
-                    </div>
-                </div>
+    <div className={`nt-money-converter nt-component ${className}`}>
+      <form>
+        <div className="flex">
+          <div className="nt-form-col pl-2 grow">
+            <label className="nt-label" htmlFor="nt-base-currency">
+              How much you wanna trade?
+            </label>
+            <div className="border rounded p-1 flex">
+              <input type="number" className="grow" defaultValue={0} onChange={(e)=>{setAmount(parseFloat(e.target.value))}}/>
+              <select name="nt-currency-select" onChange={(e)=>{setRate(parseFloat(e.target.value))}}>
+                <option key="select-currency" value={0}>Currency</option>
+                {data.map((item, index) => (
+                  <option
+                    key={`nt-country-${index}`}
+                    value={item.we_sell}
+                  >
+                    {item.flag} {item.code}
+                  </option>
+                ))}
+              </select>
             </div>
-            {/*  */}
-            <button className="bg-black text-white rounded py-2 px-4 mx-auto flex mt-3" type="submit">Convert</button>
-        </form>
+            <div className="text-center">
+              The rate is {rate}, Te amount is {amount}, You will get <span>{rate * amount}</span>
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        <button className="bg-black text-white rounded py-2 px-4 mx-auto flex mt-3" type="submit">
+          Order
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MoneyConverter
+export default MoneyConverter;
