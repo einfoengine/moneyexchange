da6fbc858cb8427ccd604ddf8134229010b3b7fc
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2023-09-09 06:59:17 +0800

    On going style

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,6 +1,6 @@
 const Banner = ({className=''}:{className: string}) => {
     return(
-        <div className={`nt-component nt-hero ${className}`}>
+        <div className={`nt-component nt-hero-message ${className}`}>
             <h3 className='text-3xl'>An effordable, rapid method for intarnational <span>currency exchange!</span></h3>
             <p>We are here serving you the best in Malaysia</p>
             <button>See how it works</button>
